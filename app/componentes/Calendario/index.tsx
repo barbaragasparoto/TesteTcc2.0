f35c2592b40d14c71e1e9529bc0933ded92df42d
barbaragasparoto <barbaragasparotofigeuierdo@gmail.com>
2023-10-26 12:22:26 -0300

    calendario atualizado

--- app/componentes/Calendario/index.tsx
+++ app/componentes/Calendario/index.tsx
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
 import DatePicker from 'react-native-modern-datepicker';
+import {addDays, isWeekend, } from "date-fns"
 
  function Calendario() {
 const [selectedDate, setSelectedDate] = useState("");
 
-   const handleDateChange = (date) => {
-    setSelectedDate(date);
-  };
+  const handleDateChange = (date) => {setSelectedDate(date);}
+  const minimumDate = "2024-01-31";
+  const maximumDate = "2025-01-31";
+  const isWeekendDay = (date) => {return isWeekend(date);}
+  const filterWeekends = (date) => {return !isWeekendDay(date);}
 
 
 
@@ -15,8 +18,9 @@ const [selectedDate, setSelectedDate] = useState("");
        <DatePicker
        selected={selectedDate}
        onDateChange={handleDateChange}
+       minimumDate={minimumDate}
+       maximumDate={maximumDate}
       
-       //TimeIntervals={10}
        />
   
   );
