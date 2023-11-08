65b8c54008b70c75e087e9598a117c2949ec5f8d
barbaragasparotofigueiredo@gmail.com <barbaragasparotofigueiredo@gmail.com>
2023-11-08 16:01:49 -0300

    08/11

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,21 @@
 import React from "react";
+
 import { Stack } from "expo-router";
 import { StatusBar } from "react-native";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 export default function AppLayout() {
+
+  const queryClient = new QueryClient()
+
+  
   return (
+    <QueryClientProvider client={queryClient}>
     <Stack>
       <StatusBar />
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       <Stack.Screen name="Login" options={{ headerShown: false }} />
     </Stack>
+    </QueryClientProvider>
   );
 }
--- app/componentes/Calendario/index.tsx
+++ app/componentes/Calendario/index.tsx
@@ -4,18 +4,22 @@ import {addDays, isWeekend, } from "date-fns";
 import { StyleSheet } from "react-native";
 import { Center } from "@mantine/core";
 import { CSSProperties } from "react";
+import useCustomFetch from "../../hooks/useFetch";
 
 
  function Calendario() {
 const [selectedDate, setSelectedDate] = useState("");
+const [time, setTime] = useState("");
 
   const handleDateChange = (date) => {setSelectedDate(date);}
+  const handleTimeChange = (time) => {setTime(time);}
   const minimumDate = "2024-01-31";
   const maximumDate = "2025-01-31";
   const isWeekendDay = (date) => {return isWeekend(date);}
   const filterWeekends = (date) => {return !isWeekendDay(date);}
+  const [date, useDate] = useState();
 
-
+  console.log(time)
 
   return (
     
@@ -23,14 +27,16 @@ const [selectedDate, setSelectedDate] = useState("");
        options={{selectedTextColor: "#ffff", mainColor: "#462783", daysAnimationDistance:20} }
        style={estilos.data}
        selected={selectedDate}
+       onTimeChange={handleTimeChange}
        onDateChange={handleDateChange}
        minimumDate={minimumDate}
        maximumDate={maximumDate}
-       
        />
   
   );
 };
+
+
 
 const estilos = StyleSheet.create({
   data: {
