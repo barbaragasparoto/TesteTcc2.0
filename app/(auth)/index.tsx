54f42abbdc8bba386e5649027cf6d229e0711432
barbaragasparoto <barbaragasparotofigueiredo@gmail.com>
2023-11-24 09:06:49 -0300

    novo

--- app/(auth)/index.tsx
+++ app/(auth)/index.tsx
@@ -1,112 +1,135 @@
 import React, { useState } from "react";
-import { StyleSheet, View, TextInput, Text, Pressable } from "react-native";
-import Logo from "../componentes/Logo";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  Alert,
+} from "react-native";
+import { FontAwesome5 } from "@expo/vector-icons";
+import { Picker } from "@react-native-picker/picker";
 import { useRouter } from "expo-router";
-import { Input } from "@rneui/themed";
+import Logo from "../componentes/Logo";
 
-export default function Login() {
-  const [text] = useState("");
-  const router = useRouter();
+const Header = () => (
+  <Logo />
+);
+
+function UserRegistration() { 
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
-  const [senha, setSenha] = useState("");
+  const [selectedCity, setSelectedCity] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+  const [isConfirmPasswordVisible, setIsConfirmPasswordVisible] =
+    useState(false);
 
-  const validateFields = () => {
-    const errors = { email, senha };
+  const router = useRouter();
 
-    if (!email) {
-      errors.email = "O campo usuário é obrigatório.";
-    } else {
-      errors.email = "";
-    }
-
-    const regexPassword =
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
-    if (!senha) {
-      errors.senha = "A senha é obrigatória.";
-    } else {
-      errors.senha = "";
-    }
-
-    return errors;
+  const togglePasswordVisibility = () => {
+    setIsPasswordVisible(!isPasswordVisible);
   };
 
-  const error = validateFields();
+  const toggleConfirmPasswordVisibility = () => {
+    setIsConfirmPasswordVisible(!isConfirmPasswordVisible);
+  };
+
+  const handleRegistration = () => {
+    if (
+      name === "" ||
+      password === "" ||
+      confirmPassword === "" ||
+      selectedCity === ""
+    ) {
+      Alert.alert("Aviso", "Todos os campos são obrigatórios!");
+      return;
+    }
+    // aqui ele ve se a senha tem 6 caracteres
+    if (password.length < 6) {
+      Alert.alert("Aviso", "A senha deve ter pelo menos 6 caracteres!");
+      return;
+    }
+
+    // aqui ele ve se o confirmar senha esta igual a senha
+    if (password !== confirmPassword) {
+      Alert.alert("Aviso", "As senhas não coincidem!");
+      return;
+    }
+    console.log("Cidade selecionada:", selectedCity);
+
+    setName("");
+    setSelectedCity("");
+    setPassword("");
+    setConfirmPassword("");
+
+    Alert.alert("Sucesso", "Os dados foram salvos com sucesso!");
+
+    router.replace("(auth)");
+  };
 
   return (
-    <View style={estilos.container}>
-      <Logo />
-
-      <View style={estilos.conteudo}>
-        <Input
-          errorMessage={error.email}
-          value={email}
-          onChangeText={setEmail}
-          style={estilos.input2}
-          placeholder="Email:"
-          placeholderTextColor="white"
+    <View style={styles.container}>
+      <Header />
+      <View >
+       
+        <TextInput
+          placeholder="Nome de usuário"
+          value={name}
+          onChangeText={(text) => setName(text)}
+          style={[styles.input, styles.passwordInput1, name !== "" && styles.validInput]}
         />
 
-        <Input
-          errorMessage={error.senha}
-          value={senha}
-          onChangeText={setSenha}
-          secureTextEntry={true}
-          style={estilos.input2}
-          placeholder="Senha:"
-          placeholderTextColor="white"
-        />
-
-        <Text style={estilos.texto}>{text}</Text>
-
-        <Pressable
-          style={estilos.botao}
+        <View style={styles.passwordContainer}>
+          <TextInput
+            placeholder="Senha"
+            secureTextEntry={!isPasswordVisible}
+            value={password}
+            onChangeText={(text) => setPassword(text)}
+            style={[styles.passwordInput, password !== "" && styles.validInput]}
+          />
+          <TouchableOpacity
+            onPress={togglePasswordVisibility}
+            style={styles.eyeIcon}
+          >
+            <FontAwesome5
+              name={isPasswordVisible ? "eye-slash" : "eye"}
+              size={20}
+              color="#888"
+            />
+          </TouchableOpacity>
+        </View>
+        
+        
+      </View>
+      <TouchableOpacity style={styles.botao}
           onPress={() => router.replace("(tabs)")}
         >
-          <Text style={estilos.texto}>Entrar</Text>
-        </Pressable>
-        <Pressable onPress={() => router.replace("Solicitacao")}>
-          <Text style={estilos.texto1} background-color="blue">
+          <Text style={styles.texto}>Entrar</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => router.replace("Solicitacao")}>
+          <Text style={styles.texto1} background-color="blue">
             Cadastre-se
           </Text>
-        </Pressable>
-      </View>
+        </TouchableOpacity>
     </View>
   );
 }
 
-const estilos = StyleSheet.create({
-  container: {
-    backgroundColor: "black",
-    height: "100%",
-  },
-  
+const styles = StyleSheet.create({
 
-  input2: {
-    color: "white",
-  },
-
-  input: {
-    width: "80%",
-    height: 40,
-    borderRadius: 20,
-    borderWidth: 1,
-    padding: 10,
-    color: "white",
-    backgroundColor: "#462783",
-    paddingStart: 10,
-    marginTop: 10,
-  },
-
-  texto: {
-    color: "white",
-    textAlign: "center",
-  },
 
   texto1: {
     color: "#2395FF",
     textAlign: "center",
     marginTop: 9,
     textDecorationLine: "underline",
+  },
+
+  texto: {
+    color: "white",
+    textAlign: "center",
   },
 
   botao: {
@@ -116,10 +139,76 @@ const estilos = StyleSheet.create({
     marginTop: 5,
     width: "50%",
   },
-  conteudo: {
-    display: "flex",
+
+  container: {
+    flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    flexDirection: "column",
+    backgroundColor: "black",
+  },
+  card: {
+    borderWidth: 10,
+    backgroundColor: "white"
+  },
+ 
+  input: {
+    height: 40,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 10,
+    marginBottom: 10,
+    backgroundColor: "white",
+
+  },
+  validInput: {
+    borderColor: "green",
+    
+  },
+  passwordContainer: {
+    flexDirection: "row",
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 10,
+    marginBottom: 10,
+    backgroundColor: "white",
+    width: "80%",
+
+
+  },
+
+  passwordInput1: {
+    paddingHorizontal: 10,
+  },
+  passwordInput2: {
+    paddingHorizontal: 10,
+  },
+  passwordInput: {
+    flex:1,
+    backgroundColor: "white",
+    height: 40,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 10,
+    paddingHorizontal: 10,
+  },
+  eyeIcon: {
+    padding: 1,
+    backgroundColor: "white",
+    borderColor: "#ccc",
+    borderRadius: 10,
+    marginBottom: 10,
+marginTop: 10  },
+  button: {
+    backgroundColor: "#462783",
+    borderRadius: 20,
+    padding: 10,
+    marginTop: 10,
+    width: "50%",
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
   },
 });
+
+export default UserRegistration;
